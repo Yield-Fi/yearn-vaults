c9a19269d4817e3d7affe069d7bf7ba8a0cc6131
HeroBorg <iherondev@gmail.com>
2023-02-08 23:59:20 +0100

    chore: cleanup VaultConfig
    Removed unnecessary variables, used external instead of public to save gas

--- contracts/VaultConfig.sol
+++ contracts/VaultConfig.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./BaseStrategy.sol";
 
 contract VaultConfig {
     using SafeMath for uint256;
@@ -10,14 +11,13 @@ contract VaultConfig {
     address public pendingGovernance;
     address public management;
     address public guardian;
-    address public partner;
-    address public partnerFeeRecipient;
-    address public approver;
+    address public partner; //address that collects this partner's rewards
+    address public approver; //address that controls this partner's vaults whitelists
     address public rewards;
 
     mapping (address => bool) isWhitelisted;
 
-    mapping (address => uint256) partnerFees;
+    mapping (address => uint256) partnerFees;//TODO replace these with vaults controlled by this VaultConfig
     mapping (address => uint256) managementFees;
     mapping (address => uint256) performanceFees;
 
@@ -31,22 +31,25 @@ contract VaultConfig {
     event ManagementFeeUpdated(address indexed vault, uint256 newFee);
     event PartnerFeeUpdated(address indexed vault, uint256 newFee);
     event PartnerUpdated(address indexed from, address indexed partner);
-    event PartnerFeeRecipientUpdated (address indexed from, address indexed recipient);
     event ApproverUpdated (address indexed from, address indexed approver);
     event GuardianUpdated (address indexed from, address indexed guardian);
     event GovernanceUpdated (address indexed newGov, address indexed oldGov);
     event RewardRecipientUpdated (address indexed reward);
 
     modifier onlyGov () {
-        require(msg.sender == governance, "feeConfig/unauthorised gov");
+        require(msg.sender == governance, "!Gov");
         _;
     }
     modifier onlyPartner () {
-        require(msg.sender == partner, "feeConfig/unauthorised partner");
+        require(msg.sender == partner, "!Partner");
         _;
     }
     modifier onlyPendingGov () {
-        require(msg.sender == pendingGovernance, "feeConfig/not a pendng gov");
+        require(msg.sender == pendingGovernance, "!PendingGov");
+        _;
+    }
+    modifier onlyApprover () {
+        require(msg.sender == approver, "!Approver");
         _;
     }
 
@@ -54,9 +57,8 @@ contract VaultConfig {
         governance = msg.sender;
     }
 
-    function config (address _partner, address _management, address _guardian, address _rewards, address _approver) public onlyGov {
+    function config (address _partner, address _management, address _guardian, address _rewards, address _approver) external onlyGov {
         partner = _partner;
-        partnerFeeRecipient = _partner;
         management = _management;
         guardian = _guardian;
         rewards = _rewards;
@@ -64,123 +66,97 @@ contract VaultConfig {
         emit Configured(_partner, _management, _guardian, _rewards, _approver);
     }
 
-    function updatePartner (address _partner ) public onlyGov {
+    function updatePartner (address _partner ) external onlyGov {//FIXME onlyGov, onlyPartner, onlyApprover?
         partner = _partner;
-        partnerFeeRecipient = _partner;
         emit PartnerUpdated(msg.sender ,_partner);
     }
 
-    function updatePartnerFeeRecipient (address _recipient) public onlyPartner {
-        partnerFeeRecipient = _recipient;
-        emit PartnerFeeRecipientUpdated(msg.sender, _recipient);
-    }
-
-    function updatePartnerFee (address _vault, uint256 _partnerFee) public {
-        require(
-            msg.sender == governance ||
-            msg.sender ==  partner,
-            "feeConfig/unauthorised to update partner fee"
-        );
-        require(MAX_BPS > _partnerFee, "feeConfig/invalid partner fee");
+    function updatePartnerFee (address _vault, uint256 _partnerFee) external onlyPartner {//FIXME onlyGov, onlyPartner, onlyApprover?
+        require(MAX_BPS > _partnerFee, "VaultConfig: invalid partner fee");
         partnerFees[_vault] = _partnerFee;
         emit PartnerFeeUpdated(_vault, _partnerFee);
     }
 
-    function updatePerformanceFee (address _vault,uint256 _performanceFee) public onlyGov {
-        require(MAX_BPS.div(2) > _performanceFee, "feeConfig/invalid managementFee");
+    function updatePerformanceFee (address _vault,uint256 _performanceFee) external onlyGov {
+        require(MAX_BPS.div(2) > _performanceFee, "VaultConfig:invalid managementFee");
         performanceFees[_vault] = _performanceFee;
         emit PerformanceFeeUpdated(_vault,_performanceFee);
     }
 
-    function updateManagement (address _newManagement) public onlyGov {
-        require(_newManagement != address(0), "feeConfig/invalid management address");
+    function updateManagement (address _newManagement) external onlyGov {
+        require(_newManagement != address(0), "VaultConfig:invalid management address");
         management = _newManagement;
         emit ManagementUpdated(msg.sender, _newManagement);
     }
 
-    function updateManagementFee (address _vault, uint256 _managementFee) public onlyGov {
-        require(MAX_BPS > _managementFee, "feeConfig/invalid managementFee");
+    function updateManagementFee (address _vault, uint256 _managementFee) external onlyGov {
+        require(MAX_BPS > _managementFee, "VaultConfig:invalid managementFee");
         managementFees[_vault] = _managementFee;
         emit ManagementFeeUpdated(_vault,_managementFee);
     }
 
-    function updateGuardian (address _newGuardian) public onlyGov {
-        require(_newGuardian != address(0), "feeConfig/invalid guardian");
+    function updateGuardian (address _newGuardian) external onlyGov { //FIXME one guardian for all the vaults?
+        require(_newGuardian != address(0), "VaultConfig:invalid guardian");
         guardian = _newGuardian;
         emit GuardianUpdated(msg.sender, _newGuardian);
     }
 
-    function updateApprover (address _newApprover) public onlyGov {
-        require(_newApprover != address(0), "feeConfig/invalid approver");
+    function updateApprover (address _newApprover) external onlyGov { //FIXME onlyGov, onlyPartner, onlyApprover?
+        require(_newApprover != address(0), "VaultConfig:invalid approver");
         approver = _newApprover;
         emit ApproverUpdated(msg.sender, _newApprover);
     }
-    function updateRewards (address _newRewards) public onlyGov {
-        require(_newRewards != address(0), "feeConfig/invalid reward recipient");
+    function updateRewards (address _newRewards) external onlyGov {
+        require(_newRewards != address(0), "VaultConfig:invalid reward recipient");
         rewards = _newRewards;
         emit RewardRecipientUpdated(_newRewards);
     }
 
-    function proposeNewGoverner (address newGov) public onlyGov {
+    function proposeNewGoverner (address newGov) external onlyGov {
         pendingGovernance = newGov;
     }
 
-    function acceptGovernance () public onlyPendingGov {
+    function acceptGovernance () external onlyPendingGov {
         emit GovernanceUpdated(pendingGovernance, governance);
         governance = pendingGovernance;
         pendingGovernance = address(0);
     }
 
-    function getManagementFee (address _vault) public view returns (uint256 fee) {
-        fee = managementFees[_vault];
-        if(fee == 0)
-            fee = 200;
+    function getManagementFee (address _vault) external view returns (uint256) {
+        return managementFees[_vault];
     }
 
-    function getPerformanceFee (address _vault) public view returns (uint256 fee) {
-        fee = performanceFees[_vault];
-        if (fee == 0)
-            fee = 1000;
+    function getPerformanceFee (address _vault) external view returns (uint256) {
+        return performanceFees[_vault];
     }
 
-    function getPartnerFee (address _vault) public view returns (uint256 fee) {
-        fee = partnerFees[_vault];
+    function getPartnerFee (address _vault) external view returns (uint256) {
+        return partnerFees[_vault];
     }
 
-    function whitelist (address _toWhitelist) public {
-        require(
-            msg.sender == governance ||
-            msg.sender ==  partner,
-            "feeConfig/unauthorised to whitelist"
-        );
-        require(_toWhitelist != address(0), "feeConfig/invalid address to whitelist");
-        require(!isWhitelisted[_toWhitelist], "feeConfig/already whitelisted");
+    function whitelist (address _toWhitelist) external onlyApprover {
+        require(_toWhitelist != address(0), "VaultConfig:invalid address to whitelist");
+        require(!isWhitelisted[_toWhitelist], "VaultConfig:already whitelisted");
         isWhitelisted[_toWhitelist] = true;
     }
 
-    function cancelWhitelist (address _toCancel) public {
-        require(
-            msg.sender == governance ||
-            msg.sender ==  partner,
-            "feeConfig/unauthorised to whitelist"
-        );
-        require(_toCancel != address(0), "feeConfig/invalid address to cancel whitelisting");
+    function cancelWhitelist (address _toCancel) external onlyApprover {
+        require(_toCancel != address(0), "VaultConfig:invalid address to cancel whitelisting");
         isWhitelisted[_toCancel] = false;
     }
 
-    function bulkWhitelist (address [] calldata _toWhitelists) public {
-        require(
-            msg.sender == governance ||
-            msg.sender ==  partner,
-            "feeConfig/unauthorised to whitelist"
-        );
-        uint256 length = _toWhitelists.length;
-        for (uint256 i = 0; i < length ;i++) {
-            address tmp = _toWhitelists[i];
-            if(tmp != address(0) && isWhitelisted[tmp]) {
-                isWhitelisted[tmp] = true;
-            }
+    function bulkWhitelist (address [] calldata _toWhitelists) external onlyApprover {
+        for (uint256 i = 0; i < _toWhitelists.length ;i++) {
+            require(_toWhitelists[i] != address(0), "!Zero");
+            isWhitelisted[tmp] = true;
         }
+    }
+
+    function bulkCancelWhitelist (address [] calldata _toCancel) external onlyApprover {
+        for (uint256 i = 0; i < _toWhitelists.length ;i++) {
+            require(_toWhitelists[i] != address(0), "!Zero");
+            isWhitelisted[tmp] = false;
+        }   
     }
 
 }
