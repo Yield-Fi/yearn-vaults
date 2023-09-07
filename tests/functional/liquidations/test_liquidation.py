8d6619de0a2f2795ef02cf1cc4746411c352dc5f
degenRobot <86513395+degenRobot@users.noreply.github.com>
2023-09-07 17:28:54 +0800

    added revoke liquidation test

--- tests/functional/liquidations/test_liquidation.py
+++ tests/functional/liquidations/test_liquidation.py
@@ -2,7 +2,7 @@ import pytest
 import brownie
 from brownie import ZERO_ADDRESS
 from contextlib import contextmanager
-
+from brownie import chain
 
 @pytest.fixture
 def vault(gov, token, Vault, liquidator):
@@ -38,9 +38,51 @@ def test_deposit_and_liquidate(gov, vault, token, liquidator):
     with brownie.reverts():
         vault.liquidate(vault.balanceOf(gov), gov, {"from": liquidator})
 
+
     # Gov sets liquidation allowance to total balance 
     vault.adjustLiquidationRequest(vault.balanceOf(gov),  {"from": gov})
+    assert vault.pendingLiquidatons(gov) == vault.balanceOf(gov)
+
+
     vault.liquidate(vault.balanceOf(gov), gov, {"from": liquidator})
+    assert vault.pendingLiquidatons(gov) == 0 
     assert vault.totalSupply() == token.balanceOf(vault) == 0
     assert vault.totalDebt() == 0
     assert token.balanceOf(gov) == balance
+
+
+def test_deposit_and_revoked_liquidate(gov, vault, token, liquidator):
+    balance = token.balanceOf(gov)
+    token.approve(vault, balance, {"from": gov})
+    vault.deposit(balance // 2, {"from": gov})
+
+    assert vault.liquidator() == liquidator
+    assert token.balanceOf(vault) == balance // 2
+    assert vault.totalDebt() == 0
+    assert vault.pricePerShare() == 10 ** token.decimals()  # 1:1 price
+
+    # Do it twice to test behavior when it has shares
+    vault.deposit({"from": gov})
+
+    assert vault.totalSupply() == token.balanceOf(vault) == balance
+    assert vault.totalDebt() == 0
+    assert vault.pricePerShare() == 10 ** token.decimals()  # 1:1 price
+
+    # Can't liquidate as gov has't allowed liquidations 
+    with brownie.reverts():
+        vault.liquidate(vault.balanceOf(gov), gov, {"from": liquidator})
+
+
+    # Gov sets liquidation allowance to total balance 
+    vault.adjustLiquidationRequest(vault.balanceOf(gov),  {"from": gov})
+    assert vault.pendingLiquidatons(gov) == vault.balanceOf(gov)
+
+    chain.mine(5)
+    chain.sleep(5)
+
+    vault.adjustLiquidationRequest(0,  {"from": gov})
+
+    with brownie.reverts():
+        vault.liquidate(vault.balanceOf(gov), gov, {"from": liquidator})
+
+    assert vault.pendingLiquidatons(gov) == 0 
